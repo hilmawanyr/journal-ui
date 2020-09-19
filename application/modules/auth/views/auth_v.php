fbab1777ae982c79d600c6eecf35a11124a26b3a
hilmawanyr <hilmawanyr@gmail.com>
2020-09-20 05:45:48 +0700

    disable signup and change app name on login page

--- application/modules/auth/views/auth_v.php
+++ application/modules/auth/views/auth_v.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>UI.Journal | Log in</title>
+  <title>MRSD | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="<?= base_url('auth') ?>"><b>UI</b>Journal</a>
+    <a href="<?= base_url('auth') ?>"><b>Metadata</b> Resource</a>
   </div>
 
   <?php if ($this->session->flashdata('wrong_password')) : ?>
@@ -101,7 +101,7 @@
     </form>
     <br>
     <a href="#myModal" data-toggle="modal">I forgot my password</a><br>
-    <a href="<?= base_url('signup') ?>" class="text-center">Register a new membership</a>
+    <!-- <a href="<?= base_url('signup') ?>" class="text-center">Register a new membership</a> -->
 
   </div>
   <!-- /.login-box-body -->
